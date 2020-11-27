82be9386c7a707ad182f5e66264b2623e01598ef
bessfernandez <bessfernandez@gmail.com>
2020-11-27 13:32:19 -0800

    Updating schema, adding profile

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '../theme'
 import '../styles/tailwind.css'
+import '../styles/globals.css'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props
