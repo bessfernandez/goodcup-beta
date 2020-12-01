98ccc6a358474bc1b1381fcade735e63b737dc39
bessfernandez <bessfernandez@gmail.com>
2020-11-30 16:49:51 -0800

    Fixing TS

--- pages/test-template.tsx
+++ pages/test-template.tsx
@@ -592,7 +592,7 @@ const TestTemplate = ({ token }) => {
                           <textarea
                             id="about"
                             name="about"
-                            rows="3"
+                            rows={3}
                             className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
                           ></textarea>
                         </div>
