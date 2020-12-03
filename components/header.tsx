77695cfb070b8c14b841d0088531b65e75e42115
bessfernandez <bessfernandez@gmail.com>
2020-12-02 18:20:55 -0800

    Fixing up home page

--- components/header.tsx
+++ components/header.tsx
@@ -52,21 +52,21 @@ const Header = () => {
                       href="#"
                       className="rounded-md py-2 px-3 text-sm font-medium text-white hover:bg-indigo-800"
                     >
-                      Jobs
+                      Coffees
                     </a>
 
                     <a
                       href="#"
                       className="rounded-md py-2 px-3 text-sm font-medium text-white hover:bg-indigo-800"
                     >
-                      Applicants
+                      Roasters
                     </a>
 
                     <a
                       href="#"
                       className="rounded-md py-2 px-3 text-sm font-medium text-white hover:bg-indigo-800"
                     >
-                      Company
+                      Your Cups
                     </a>
                   </div>
                 </div>
@@ -187,7 +187,7 @@ const Header = () => {
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="rounded-full h-8 w-8"
-                          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80"
+                          src="https://avatars0.githubusercontent.com/u/267047?s=460&v=4"
                           alt=""
                         />
                       </button>
@@ -256,21 +256,21 @@ const Header = () => {
                 href="#"
                 className="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-indigo-800"
               >
-                Jobs
+                Coffees
               </a>
 
               <a
                 href="#"
                 className="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-indigo-800"
               >
-                Applicants
+                Roasters
               </a>
 
               <a
                 href="#"
                 className="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-indigo-800"
               >
-                Company
+                Your Cups
               </a>
             </div>
             <div className="pt-4 pb-3 border-t border-indigo-800">
@@ -278,7 +278,7 @@ const Header = () => {
                 <div className="flex-shrink-0">
                   <img
                     className="rounded-full h-10 w-10"
-                    src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80h"
+                    src="https://avatars0.githubusercontent.com/u/267047?s=460&v=4"
                     alt=""
                   />
                 </div>
@@ -364,276 +364,12 @@ const Header = () => {
         </div>
         <header className="relative py-10">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-white">Settings</h1>
+            <h1 className="hidden text-3xl font-bold text-white">
+              Header here hidden or not
+            </h1>
           </div>
         </header>
       </div>
-      {/* 
-      <nav className="bg-indigo-600">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <FontAwesomeIcon
-                  size="2x"
-                  color="bg-indigo-100"
-                  className="text-white"
-                  icon={faCoffeeTogo}
-                />
-              </div>
-
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link href="/">
-                    <a className="px-3 py-2 rounded-md text-sm font-medium text-indigo-800 bg-indigo-100 focus:outline-none focus:text-white focus:bg-coolGrey-100">
-                      Home
-                    </a>
-                  </Link>
-
-                  <Link href="/">
-                    <a className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                      Your Coffees
-                    </a>
-                  </Link>
-
-                  <Link href="/">
-                    <a className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                      Top Picks
-                    </a>
-                  </Link>
-
-                  <Link href="/">
-                    <a className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                      Roasters
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6">
-                {user ? (
-                  <React.Fragment>
-                    <Link href="/profile">
-                      <a className="p-10 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                        {user.email}
-                      </a>
-                    </Link>
-                  </React.Fragment>
-                ) : (
-                  <React.Fragment>
-                    <Link href="/login">
-                      <a className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                        Login
-                      </a>
-                    </Link>
-                    <Link href="/signup">
-                      <a className="p-5 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">
-                        Signup
-                      </a>
-                    </Link>
-                  </React.Fragment>
-                )}
-
-                <button
-                  className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                  aria-label="Notifications"
-                >
-                  <svg
-                    className="h-6 w-6"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    />
-                  </svg>
-                </button>
-
-                <div className="ml-3 relative">
-                  <div>
-                    <button
-                      className="max-w-xs flex items-center text-sm rounded-full text-white focus:outline-none focus:shadow-solid"
-                      id="user-menu"
-                      aria-label="User menu"
-                      aria-haspopup="true"
-                      onClick={(e) => setDropOpen(!isDropOpen)}
-                    >
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://avatars0.githubusercontent.com/u/267047?s=460&v=4"
-                        alt="Profile page"
-                      />
-                    </button>
-                  </div>
-                  {isDropOpen && (
-                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
-                      <div
-                        className="py-1 rounded-md bg-white shadow-xs"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="user-menu"
-                      >
-                        <Link href="/login">
-                          <a
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            role="menuitem"
-                          >
-                            Login
-                          </a>
-                        </Link>
-                        <Link href="/profile">
-                          <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            role="menuitem"
-                          >
-                            Your Profile
-                          </a>
-                        </Link>
-
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          role="menuitem"
-                        >
-                          Settings
-                        </a>
-
-                        <button
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          onClick={logout}
-                        >
-                          Logout
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={(e) => setHamburgerOpen(!isHamburgerOpen)}
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:bg-gray-700 focus:text-white"
-              >
-                <svg
-                  className="block h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-                <svg
-                  className="hidden h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {isHamburgerOpen && (
-          <div>
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-              >
-                Dashboard
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              >
-                Team
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              >
-                Projects
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              >
-                Calendar
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-              >
-                Reports
-              </a>
-            </div>
-            <div className="pt-4 pb-3 border-t border-gray-700">
-              <div className="flex items-center px-5 space-x-3">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://avatars0.githubusercontent.com/u/267047?s=460&v=4"
-                    alt="testing"
-                  />
-                </div>
-                <div className="space-y-1">
-                  <div className="text-base font-medium leading-none text-white">
-                    Bess Fernandez
-                  </div>
-                  <div className="text-sm font-medium leading-none text-gray-400">
-                    bess@example.com
-                  </div>
-                </div>
-              </div>
-              <div className="mt-3 px-2 space-y-1">
-                <a
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  Your Profile
-                </a>
-
-                <a
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  Settings
-                </a>
-
-                <a
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          </div>
-        )}
-      </nav> */}
     </header>
   )
 }
