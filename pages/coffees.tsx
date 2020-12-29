3cb385cd2cc06e53f2ebddd4ee457d18281cd38d
bessfernandez <bessfernandez@gmail.com>
2020-12-29 12:16:59 -0800

    Basic detail layout and ratings

--- pages/coffees.tsx
+++ pages/coffees.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useState } from 'react'
 import Layout from '../components/layout/layout'
 import DisqusThread from '../components/disqus-thread'
-import utilStyles from '../styles/utils.module.css'
+import ratingStyles from '../styles/ratings.module.css'
 
 const CoffeeDetail = () => {
   return (
@@ -14,54 +13,43 @@ const CoffeeDetail = () => {
               <div className="divide-y divide-gray-200 lg:col-span-9">
                 <div className="py-6 px-4 sm:p-6 lg:pb-8">
                   <div>
-                    <header>
-                      <h2 className="text-3xl font-bold leading-tight text-gray-900">
-                        Holiday Blend
-                      </h2>
-                      <h3>Onda</h3>
-                    </header>
-                    <div>4 starts out of 5</div>
-                    <div>
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/1607/0925/products/OG-CHR-12oz_800x800.jpg?v=1606846854"
-                        alt="hi"
-                        width="228px"
-                        height="178px"
-                      />
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/1607/0925/products/OG-CHR-12oz_800x800.jpg?v=1606846854"
-                        alt="hi"
-                        width="114px"
-                        height="89px"
-                      />
-                      <img
-                        src="https://cdn.shopify.com/s/files/1/1607/0925/products/OG-CHR-12oz_800x800.jpg?v=1606846854"
-                        alt="hi"
-                        width="114px"
-                        height="89px"
-                      />
+                    <div className="min-h-screen py-6 m-6 flex flex-col sm:py-12">
+                      <div className="justify-center bg-white pl-10">
+                        <div className="bg-white grid grid-cols-2 gap-4">
+                          <div className="mt-4">
+                            <h2 className="text-2xl">Holiday Blend</h2>
+                            <h3 className="mt-2 text-base">Klatch Coffee</h3>
+                          </div>
+                          <div className="mt-4">
+                            <div className={ratingStyles.rating}>
+                              <span>☆</span>
+                              <span>☆</span>
+                              <span>☆</span>
+                              <span>☆</span>
+                              <span>☆</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className=" bg-white grid grid-cols-2 gap-4 pl-10">
+                        <div>
+                          <img
+                            src="https://cdn.shopify.com/s/files/1/1607/0925/products/OG-CHR-12oz_800x800.jpg?v=1606846854"
+                            alt="coffee bag"
+                          />
+                        </div>
+                        <div className="my-6">
+                          <ul className="text-base">
+                            <li>Origin: Guatamala</li>
+                            <li>Process: Washed</li>
+                            <li>Roast Level: Medium</li>
+                          </ul>
+                          <h3 className="text-xl mt-6">Flavors</h3>
+                          <p className="mt-2">Grapefruit, blackberry, honey</p>
+                        </div>
+                      </div>
                     </div>
-                    <div>
-                      <h4>Taste</h4>
-                      <p>Chart here</p>
-                    </div>
-                    <div>
-                      <p>Origin: Guatamala</p>
-                      <p>Process: Washed</p>
-                      <p>Roast Level: Medium</p>
-                    </div>
-                    <div>
-                      <h4>Flavors</h4>
-                      <p>Grapefruit, Blackberry, Honey</p>
-                    </div>
-                    <div>
-                      <h4>Notes</h4>
-                      <p>
-                        This seasonally rotating, three component espresso was
-                        crafted to pair tremendously with milk, and serve as a blend
-                        for drinkers of all types.
-                      </p>
-                    </div>
+
                     <div>
                       <DisqusThread post={{ id: 'tbd', title: 'Coffee detail' }} />
                     </div>
