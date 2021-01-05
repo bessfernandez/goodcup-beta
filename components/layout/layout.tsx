86ae8f4853c065759dff4e7bf23024212a5824e1
bessfernandez <bessfernandez@gmail.com>
2021-01-05 15:58:21 -0800

    Remove old TODO code

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
-import styles from './layout.module.css'
 import Header from '../header'
 
 const Layout = ({ children }) => (
   <React.Fragment>
     <Head>
-      <title>Good cup</title>
+      <title>Good cup - Your favorite cups</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <Header />
 
     <main>
-      <div className={styles.container}>{children}</div>
+      <div>{children}</div>
     </main>
   </React.Fragment>
 )
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import Layout from '../components/layout/layout'
-import utilStyles from '../styles/utils.module.css'
 
 const Login = () => {
   const router = useRouter()
@@ -37,65 +36,43 @@ const Login = () => {
 
   return (
     <Layout>
+      {/* Basic mostly unstyled form for testing login */}
       <main className="relative -mt-32">
         <div className="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:pb-16 lg:px-8">
           <div className="bg-white rounded-lg shadow overflow-hidden">
-            <div className="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
-              <div className="divide-y divide-gray-200 lg:col-span-9">
-                <div className="py-6 px-4 sm:p-6 lg:pb-8">
-                  <header>
-                    <h2 className="mb-6 text-3xl font-bold leading-tight text-gray-900">
-                      Log in
-                    </h2>
-                  </header>
+            <header>
+              <h2 className="mb-6 text-3xl">Log in</h2>
+            </header>
 
-                  <form onSubmit={onSubmit} className={utilStyles.form}>
-                    <div>
-                      <label htmlFor="login-email">Email</label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="login-email"
-                        ref={register({ required: 'Email is required' })}
-                      />
-                      {errors.email && (
-                        <span role="alert" className={utilStyles.error}>
-                          {errors.email.message}
-                        </span>
-                      )}
-                    </div>
-
-                    <div>
-                      <label htmlFor="user-password">Password</label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="user-password"
-                        ref={register({ required: 'Password is required' })}
-                      />
-                      {errors.password && (
-                        <span role="alert" className={utilStyles.error}>
-                          {errors.password.message}
-                        </span>
-                      )}
-                    </div>
-
-                    <button
-                      type="submit"
-                      className="bg-indigo-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Submit
-                    </button>
-                  </form>
-
-                  {errorMessage && (
-                    <p role="alert" className={utilStyles.errorMessage}>
-                      {errorMessage}
-                    </p>
-                  )}
-                </div>
+            <form onSubmit={onSubmit}>
+              <div>
+                <label htmlFor="login-email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="login-email"
+                  ref={register({ required: 'Email is required' })}
+                />
+                {errors.email && <span role="alert">{errors.email.message}</span>}
               </div>
-            </div>
+
+              <div>
+                <label htmlFor="user-password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="user-password"
+                  ref={register({ required: 'Password is required' })}
+                />
+                {errors.password && (
+                  <span role="alert">{errors.password.message}</span>
+                )}
+              </div>
+
+              <button type="submit">Submit</button>
+            </form>
+
+            {errorMessage && <p role="alert">{errorMessage}</p>}
           </div>
         </div>
       </main>
