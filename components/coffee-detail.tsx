37ad8475e4f08260b10e046d5f7c60d8f5ff7c20
bessfernandez <bessfernandez@gmail.com>
2021-01-17 15:04:37 -0800

    Simplifying grid

--- components/coffee-detail.tsx
+++ components/coffee-detail.tsx
@@ -6,17 +6,17 @@ import ratingStyles from '../styles/ratings.module.css'
 const CoffeeDetail = () => {
   return (
     <div>
-      <div className="flex md:flex-row md:justify-around sm:flex-col sm:place-items-center gap-10">
-        <div>
+      <div className="grid grid-flow-row lg:grid-flow-col med:grid-flow-col justify-items-center gap-4">
+        <div className="">
           <div>
-            <div className="grid grid-cols-1">
+            <div className="">
               <img
                 className="max-w-sm"
                 src="https://cdn.shopify.com/s/files/1/0148/9967/products/KenyaKaratu_1024x1024.png?v=1606243254"
                 alt="coffee bag"
               />
             </div>
-            <div className="grid grid-cols-2 gap-6">
+            <div className="grid grid-cols-2">
               <div className="mt-4">
                 <h2 className="text-3xl">Karatu</h2>
                 <h3 className="mt-1 text-xl">Olympia Coffee</h3>
@@ -32,7 +32,7 @@ const CoffeeDetail = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-2 row-span-2 gap-6 my-6">
+          <div className="grid grid-cols-2 mt-5">
             <div>
               <ul className="text-sm">
                 <li>Origin: Kenya</li>
@@ -44,24 +44,23 @@ const CoffeeDetail = () => {
               <h3 className="text-xl">Flavors</h3>
               <p className="mt-1">Fruit punch, herbs, and grapefruit</p>
             </div>
-            <div className="col-span-2">
-              <RadarChart />
-            </div>
+          </div>
+          <div className="grid grid-cols-1">
+            <RadarChart />
           </div>
         </div>
-
-        <div>
+        <div className="">
           <div>
-            <div className="grid grid-cols-1">
+            <div className="">
               <img
                 className="max-w-sm"
-                src="https://cdn.shopify.com/s/files/1/0148/9967/products/ColombiaAmparoPajoyMicroLot27_1024x1024.png?v=1604683105"
+                src="https://cdn.shopify.com/s/files/1/0148/9967/products/GuatemalaMartaMejiaMicroLot-WEB_5ab7cd7e-c0ac-4c48-9553-50116e0eede6_1024x1024.png?v=1602714949"
                 alt="coffee bag"
               />
             </div>
-            <div className="grid grid-cols-2 gap-6">
+            <div className="grid grid-cols-2">
               <div className="mt-4">
-                <h2 className="text-3xl">Amparao Pajay</h2>
+                <h2 className="text-3xl">Marta Mejia</h2>
                 <h3 className="mt-1 text-xl">Olympia Coffee</h3>
               </div>
               <div className="mt-6">
@@ -75,21 +74,21 @@ const CoffeeDetail = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-2 row-span-2 gap-6 my-6">
+          <div className="grid grid-cols-2 mt-5">
             <div>
               <ul className="text-sm">
-                <li>Origin: Columbia</li>
+                <li>Origin: Guatemala</li>
                 <li>Process: Washed</li>
                 <li>Roast Level: Medium</li>
               </ul>
             </div>
             <div>
               <h3 className="text-xl">Flavors</h3>
-              <p className="mt-1">Choolate, pecans, and strawberry</p>
+              <p className="mt-1">Strawberry, rose, and chocolate</p>
             </div>
-            <div className="col-span-2">
-              <RadarChart />
-            </div>
+          </div>
+          <div className="grid grid-cols-1">
+            <RadarChart />
           </div>
         </div>
       </div>
